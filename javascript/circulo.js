99149fecd25faec77641fa002f6e07d78de1d1eb
Nicolas Souza <nicolas.souza@gclaims.com.br>
2024-10-06 22:55:56 -0300

    ultimas atualizações

--- javascript/circulo.js
+++ javascript/circulo.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    const text = document.querySelector('.texto')
-    text.innerHTML = text.innerText.split('').map(
-        (char, i) => `<span style="transform:rotate(${i * 6.3}deg)">${char}</span>`
-    ).join('');
-    
-});
+    const text = document.querySelector('.texto');
+    let textArr = ['photo', '-', 'video', '-', 'design', '-', 'photo', '-', 'video', '-', 'design', '-', 'photo', '-'].map(
+      word => word + '\n'
+    );
+  
+    let textSpotted = 'photo';
+    let textIndex = 0;
+  
+    text.innerHTML = textArr.reduce((acc, word) => {
+      word.split('').forEach(char => {
+        textIndex++;
+        acc += `<span ${
+          word.match(textSpotted) == textSpotted ? 'class="spotted"' : ''
+        } style="transform:rotate(${textIndex * 6.3}deg)">${char}</span>`;
+      });
+  
+      return acc;
+    }, '');
+  });
