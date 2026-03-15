571e6cf44b39b91110dbdb680c2ab8eb138bd909
nicoCintra <nickcintra@hotmail.com>
2026-03-15 10:31:05 -0300

    removido coisas antigas

--- sitejuju-angular/src/app/components/header/header.ts
+++ sitejuju-angular/src/app/components/header/header.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, signal, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,16 +9,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit, OnDestroy {
   protected menuOpen = signal(false);
   protected dropdownOpen = signal(false);
+  private sub?: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.sub = this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(() => this.closeMenu());
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
 
   toggleMenu(): void {
     this.menuOpen.update(open => !open);
   }
 
-  toggleDropdown(event: Event): void {
-    event.preventDefault();
+  toggleDropdown(): void {
     this.dropdownOpen.update(open => !open);
   }
 
